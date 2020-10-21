7da642c58a09239ba587728b78da975412a5c20c
jinwoo <hongdongdong3012@gmail.com>
2020-10-22 01:26:29 +0900

    UI 컴포넌트 작성하기

---
+++ src/Components/App/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.css'
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <div className="title">
+          <span>Dong 심플노트</span>
+        </div>
+        <div className="buttons">
+          <button>추가</button>
+          <button>삭제</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Header;
---
+++ src/Components/App/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './index.css';
+import Header from './Header'
+import List from './List'
+import Note from './Note'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        <div className="container">
+          <List />
+          <Note />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
